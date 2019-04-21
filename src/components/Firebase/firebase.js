c174bb4919ebb75af56f84caa6db9d7993ee8783
Erik Loaiza <erik.loaiza@uao.edu.co>
2019-04-21 11:15:57 -0500

    Added firebase
    Remove basic files

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,27 @@
+import app from 'firebase/app';
+import 'firebase/auth'
+
+const config = {
+    apiKey: "AIzaSyDX3XfOXAfEbLOyXY-khxYBlcMD3996XE0",
+    authDomain: "imuao-administrador.firebaseapp.com",
+    databaseURL: "https://imuao-administrador.firebaseio.com",
+    projectId: "imuao-administrador",
+    storageBucket: "imuao-administrador.appspot.com",
+    messagingSenderId: "1000065554199"
+  };
+  firebase.initializeApp(config);
+
+  class Firebase {
+    constructor() {
+      app.initializeApp(config);
+      this.auth = app.auth();
+    }
+      // *** Auth API ***
+
+    doSignInWithEmailAndPassword = (email, password) =>
+      this.auth.signInWithEmailAndPassword(email, password);
+    
+    doSignOut = () => this.auth.signOut();
+  }
+  
+  export default Firebase;
